e6fabf9440fccf8c420f1b38fe146d119254f732
Nurcholis Art <nurcholisart@gmail.com>
2023-05-30 17:17:32 +0700

    Refine chain

--- script.py
+++ script.py
@@ -1,25 +1,27 @@
-from langchain.prompts.prompt import PromptTemplate
+import os
+import logging
+
+from langchain.chains.llm import LLMChain
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains.conversational_retrieval.prompts import (
     CONDENSE_QUESTION_PROMPT,
+    QA_PROMPT,
 )
+from langchain.chains.question_answering import load_qa_chain
+
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.chains import ConversationalRetrievalChain
-import tempfile
-import requests
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory, RedisChatMessageHistory
 
-import os
-
-import logging
-
 logging.getLogger("openai").setLevel(logging.DEBUG)  # logging.INFO or logging.DEBUG
 
+llm = OpenAI(temperature=0, verbose=True)
 
-redis_chat_history = RedisChatMessageHistory(session_id="chat_history", ttl=3600)
-
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, chat_memory=redis_chat_history)
+question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT, verbose=True)
 
 template = """You are an AI customer service agent for answering question about Qiscus. Your name is Peka.
 Use the following pieces of context to answer the question at the end.
@@ -33,30 +35,27 @@ Helpful Answer:"""
 
 prompt = PromptTemplate(template=template, input_variables=["question", "context"])
 
-vectorstore = FAISS.load_local("db", OpenAIEmbeddings())
-
-chain = ConversationalRetrievalChain.from_llm(
-    llm=OpenAI(temperature=0, max_tokens=150),
-    retriever=vectorstore.as_retriever(),
-    return_source_documents=False,
-    qa_prompt=prompt
+doc_chain = load_qa_chain(
+    llm=llm, chain_type="stuff", prompt=prompt, verbose=True
 )
 
-question = "No. Please end the chat"
+vectorstore = FAISS.load_local("db", OpenAIEmbeddings())
 
-vectordbkwargs = {"search_distance": 0.9}
+redis_chat_history = RedisChatMessageHistory(session_id="chat_history:room_1212182", ttl=3600)
+
+qa = ConversationalRetrievalChain(
+    retriever=vectorstore.as_retriever(),
+    combine_docs_chain=doc_chain,
+    question_generator=question_generator,
+    verbose=True,
+)
 
 chat_history = []
 
-result = chain(
-    {
-        "question": question,
-        "chat_history": chat_history,
-        "vectordbkwargs": vectordbkwargs
-    }
-)
+query = "Apa itu Robolabs?"
+result = qa({"question": query, "chat_history": redis_chat_history.messages})
 
-# redis_chat_history.add_user_message(result["question"])
-# redis_chat_history.add_ai_message(result["answer"])
+redis_chat_history.add_user_message(query)
+redis_chat_history.add_ai_message(result["answer"])
 
 print(result)
